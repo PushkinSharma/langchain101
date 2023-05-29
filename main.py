89763a327cb3c1e1d8b885c61df2aee493379af1
Pushkin <pushkinsharma0102@gmail.com>
2023-05-30 01:38:58 +0530

    More changes in UI

--- main.py
+++ main.py
@@ -7,23 +7,22 @@ st.title("Business Solution Provider")
 col1, col2 = st.columns(2)
 
 with col1:
-        st.markdown("Can't find the solution to your problem holding your business back? Don't worry we got you!")
+        st.markdown("Can't find the solution to the problem holding your business back? Don't worry we got you! Just enter some basic information and we will provide you with a detailed solution.")
 
 with col2:
-
         st.image(image="graphic.jpeg",width=300)
 
-st.markdown("###  Enter some details:")
+st.markdown("###  Enter some information:")
 
-col3,col4 = st.columns(2)
+col1,col2 = st.columns(2)
 
-with col3:
+with col1:
     business = st.text_input(label="",placeholder="Business Category")
 
-with col4:
+with col2:
     problem = st.text_input(label="",placeholder="Problem")
 
-
+st.markdown("### Solution:")
 
 
 
