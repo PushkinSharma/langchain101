6e1bd6558176c0f21fb939444fe6ceffdcdcf8a9
Pushkin <pushkinsharma0102@gmail.com>
2023-06-01 00:23:49 +0530

    Added content,changed image

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import streamlit as st
 st.title("Business Solution Provider")
 
 with st.sidebar:
-     st.header("Made By: Pushkin Sharma")
+     st.header("Langchain Overview: :parrot::link: ")
+     st.markdown("LangChain is a software development framework designed to simplify the creation of applications using large language models.")
+     st.header("Made By: :red[Pushkin Sharma]")
      st.write("This is my first Langchain project. I'm attempting to solve a very basic problem here.")
 
 
@@ -14,7 +16,7 @@ with col1:
         st.markdown("Can't find the solution to the problem holding your business back? Don't worry we got you! Just enter some basic information and we will provide you with a detailed solution.")
 
 with col2:
-        st.image(image="graphic.jpeg",width=400)
+        st.image(image="vecteezy_illustration-vector-graphic-cartoon-character-of-business_5482438.jpg",width=300)
 
 st.markdown("###  Enter some information:")
 
@@ -26,23 +28,17 @@ with col1:
 with col2:
     problem = st.text_input(label="",placeholder="Problem")
 
+
 st.markdown("### Solution:")
 
 
 
+st.markdown("##### Happy with the result?")
 
+col4,col5,col6,col7,col8,col9=st.columns(6)
 
+with col4:
+    st.button("Yes :smile:")
 
-
-#sidebar
-
-# add_selectbox = st.sidebar.selectbox(
-#     "How would you like to be contacted?",
-#     ("Email", "Home phone", "Mobile phone")
-# )
-#
-# with st.sidebar:
-#     add_radio = st.radio(
-#         "Choose a shipping method",
-#         ("Standard (5-15 days)", "Express (2-5 days)")
-#     )
+with col5:
+    st.button("No :japanese_goblin:")
