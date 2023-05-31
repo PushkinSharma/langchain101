bc380095a5beacb560fe80be956aac52d06d6959
Pushkin <pushkinsharma0102@gmail.com>
2023-05-31 23:56:28 +0530

    Changes to sidebar, image

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ st.title("Business Solution Provider")
 
 with st.sidebar:
      st.header("Made By: Pushkin Sharma")
-     st.write("My first langchain project")
+     st.write("This is my first Langchain project. I'm attempting to solve a very basic problem here.")
+
 
 
 col1, col2 = st.columns(2)
@@ -13,7 +14,7 @@ with col1:
         st.markdown("Can't find the solution to the problem holding your business back? Don't worry we got you! Just enter some basic information and we will provide you with a detailed solution.")
 
 with col2:
-        st.image(image="graphic.jpeg",width=300)
+        st.image(image="graphic.jpeg",width=400)
 
 st.markdown("###  Enter some information:")
 
