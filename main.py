58f757386765f0992aa45e02c8c1bc293769a486
Pushkin <pushkinsharma0102@gmail.com>
2023-06-02 00:38:06 +0530

    Added API key, updated UI and completed logic

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import streamlit as st
-import langchain
+from dotenv import load_dotenv
+from langchain import PromptTemplate
+from langchain.llms import OpenAI
 import openai
 import os
 
-template = """  """
+load_dotenv()
+api_key = os.environ.get('OPENAI_API_KEY')
 
-#prompt = PromptTemplate(input_variables=[],template = template)
+
+
+template = """ HI
+
+ business = {biz}
+
+ problem = {prob}
+
+
+
+
+ """
+
+prompt = PromptTemplate(input_variables=["biz","prob"],template = template)
+
+def load_llm():
+
+    llm = OpenAI(temperature=0.5)
+    return llm
+
+llm = load_llm()
+
+
 
 st.title("Business Solution Provider")
 
@@ -20,26 +45,50 @@ with st.sidebar:
 col1, col2 = st.columns(2)
 
 with col1:
-        st.markdown("Can't find the solution to the problem holding your business back? Don't worry we got you! Just enter some basic information and we will provide you with a detailed solution.")
+        st.markdown("Can't find the solution to the problem holding your business back? Don't worry I got you! Just enter some basic information and we will provide you with a detailed solution.")
 
 with col2:
         st.image(image="vecteezy_illustration-vector-graphic-cartoon-character-of-business_5482438.jpg",width=300)
 
 st.markdown("###  Enter some information:")
 
+
+def get_business():
+
+    business = st.text_input(label=":red[Business Category:]",placeholder="Finance")
+    return business
+
+def get_problem():
+
+    problem = st.text_input(label=":red[Problem:]",placeholder="Insufficient leads")
+    return problem
+
+
 col1,col2 = st.columns(2)
 
 with col1:
-    business = st.text_input(label="Business Category:",placeholder="Finance")
 
+    business_input = get_business()
 
 with col2:
-    problem = st.text_input(label="Problem:",placeholder="Insufficient leads")
+    problem_input = get_problem()
 
 
 st.markdown("### Solution:")
 
+if business_input and problem_input:
 
+
+    answer = llm(prompt.format(biz=business_input,prob=problem_input))
+    #st.text_area(label="",value=answer)
+    st.write(answer)
+    #st.write(prompt.format(biz=business_input,prob=problem_input))
+
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
 
 st.markdown("##### Happy with the result?")
 
